9e51b66fab6dd152cc56d106a38daedd857af5a8
Your Name <you@example.com>
2022-10-08 10:52:38 +0300

    section about work and skills

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__item-percents'),
+      lines = document.querySelectorAll('.skills__item-lineup span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
